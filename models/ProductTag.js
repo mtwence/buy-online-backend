a3dc847e56fa520f4ed5cdb167ec14e6758dfba7
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-08-01 22:47:20 -0400

    add columns to producttag model, and completed index.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,26 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      refereces: {
+        model: "product",
+        key: "id",
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      refereces: {
+        model: "tag",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
